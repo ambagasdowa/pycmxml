7811bacaadc1d95313aa385f15534cf48847fe15
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-02-21 20:00:36 -0600

    Connecting app module to db v 0.0.5

--- pycmxml/datasave/process.py
+++ pycmxml/datasave/process.py
@@ -431,8 +431,11 @@ def fetch_api( cursor, module, methods , isJson):
 
                 cursor.execute("select IDENT_CURRENT(sistemas.dbo.app_block) as id")
                 getLastBlockId = cursor.fetchone().id
+                if cursor.fetchone().id is None:
+                    print(f"getLastBlockId is none")
+                else:
+                    print(f"getLastBlockId : {getLastBlockId}")
                 cursor.commit()
-                print(f"getLastBlockId : {getLastBlockId}")
 
                 loop = 0
                 dataset = {}
