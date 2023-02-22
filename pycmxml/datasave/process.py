c17584e8680d3e03544c521470e716c4c104fd9a
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-02-22 11:19:26 -0600

    Build structure for save xml to database  v 0.0.22

--- pycmxml/datasave/process.py
+++ pycmxml/datasave/process.py
@@ -428,10 +428,11 @@ def fetch_api( cursor, module, methods , isJson):
             if tree is None:
                 print('no trees')
             else:
+
                 try:
                     request_cursor = cursor(dictionary=True)
                     db_query = request_cursor.execute("select IDENT_CURRENT(sistemas.dbo.app_block) as id")
-                    print(typeof(db_query))
+                    # print(typeof(db_query))
                     row = request_cursor.fetchone()
                     if row:
                         return json.dumps(row)
