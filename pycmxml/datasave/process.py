9a59a2997f5c61cccbcda3b992e715f27c1461cc
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-02-24 11:00:37 -0600

    Working in crud data

--- pycmxml/datasave/process.py
+++ pycmxml/datasave/process.py
@@ -443,7 +443,7 @@ def fetch_api( cursor, module, methods , isJson):
 
         created = datetime.now().isoformat(timespec='seconds')
         status= 1
-        insertBlock = 'insert into sistemas.dbo.app_block(app_methods_id,created,modified,[status]) values(?,?,?,?)'
+        insertBlock = 'insert into sistemas.dbo.app_block(app_methods_id,created,modified,status) values(?,?,?,?)'
 
         blockData = (method_id,created,'',status,)
         tableBlock = "select IDENT_CURRENT('sistemas.dbo.app_black') as id"
