e45aa7f0946db0e84db9bffbeeeb9d9a0af483c8
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-02-25 09:47:00 -0600

    Improve config.ini file

--- pycmxml/connection/db.py
+++ pycmxml/connection/db.py
@@ -1,14 +1,23 @@
 import pyodbc
-#UIX
+# UIX
 from rich import print
 from rich.progress import track
 from rich.progress import Progress
 
+# import config
+import pycmxml.config.config as conf
 
-def connect(config): 
+
+def connect(config):
+
+    config = conf.read_config()
+
     try:
         cnxn = pyodbc.connect(
-            'Trusted_Connection=no;DRIVER={'+config['db_connection']['driver']+'};SERVER='+config['db_connection']['server']+';DATABASE='+config['db_connection']['database']+';UID='+config['db_connection']['user']+';PWD='+config['db_connection']['password']
+            'Trusted_Connection=no;DRIVER={'+config['db_connection']['driver']+'};SERVER='+config['db_connection']['server']+';DATABASE=' +
+            config['db_connection']['database']+';UID=' +
+            config['db_connection']['user']+';PWD=' +
+            config['db_connection']['password']
         )
     except pyodbc.Error as e:
         # print("Error %d: %s" % (e.args[0], e.args[1]))
@@ -18,4 +27,3 @@ def connect(config):
     print("[blue]DB Connected...[blue]")
 
     return cnxn.cursor()
-
